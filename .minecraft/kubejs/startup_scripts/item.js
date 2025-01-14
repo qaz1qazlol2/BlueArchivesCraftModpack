1a097bd117e6c5448ec7a4522ddcb3a3cc63d725
qaz1qazlol2 <panyouchao1029@qaz1qazlol.onmicrosoft.com>
2025-01-14 21:00:56 +0800

    KubeJS and Tasks Update by sfhuewui
    1.任务和kjs更新合并

--- .minecraft/kubejs/startup_scripts/item.js
+++ .minecraft/kubejs/startup_scripts/item.js
@@ -5,6 +5,8 @@ onEvent('item.registry', event => {
     event.create('monster_coin').displayName('彩蛋币').tooltip('用于兑换彩蛋武器').rarity('Epic').glow(true)//发光
     event.create('diamond_coin').displayName('钻石币').tooltip('用于商店兑换以及抽奖').rarity('Epic').glow(true)//发光
     event.create('luguan').displayName('你知我说').tooltip('到点了，该做正事了').rarity('common')
+    event.create('kapian').displayName('普雷纳帕托斯的大人大片').tooltip('普雷纳帕特斯所持有的「大人的卡片」。由于卡片被烧到焦黑变形，处于随时都可能化为灰烬的状态，因此需要小心处理。使用这张卡片可以获取青辉石，但卡片会被破坏。').rarity('Epic')
+    event.create('huicheng').displayName('化为粉末的大人的卡片').tooltip('普雷纳帕特斯所持有的已经化为粉末的「大人的卡片」。这张卡片本来的样子现在只存留在某人的记忆之中。').rarity('Epic')
 
 })
 
